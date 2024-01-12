4b76de453ba286331ad0c186f7c0d86f92a7f65c
Axel Moncayo <axel.moncayo.dev@gmail.com>
2024-01-11 21:03:48 -0600

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const { src, dest, watch, series } = require("gulp");
+
+// CSS y SASS
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const sourcemaps = require("gulp-sourcemaps");
+const cssnano = require("cssnano");
+const plumber = require("gulp-plumber");
+
+// Imagenes
+const imagemin = require("gulp-imagemin");
+
+function css(done) {
+  src("src/scss/app.scss")
+    .pipe(sourcemaps.init())
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("build/css"));
+
+  done();
+}
+
+function imagenes() {
+  return src("src/img/**/*")
+    .pipe(imagemin({ optimizationLevel: 3 }))
+    .pipe(dest("build/img"));
+}
+
+function dev() {
+  watch("src/scss/**/*.scss", css);
+  watch("src/img/**/*", imagenes);
+}
+
+exports.css = css;
+exports.dev = dev;
+exports.imagenes = imagenes;
+exports.default = series(imagenes, css, dev);
